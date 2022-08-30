2547fedd9218d4af1d6606b2041463b7754c0111
nmarquillier <nmarquillier@norauto.com>
2022-08-30 14:56:59 +0200

    création + récupération des posts - ok - wip 32 min

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,74 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.id }}
+      {{ post.title }}
+      <div>
+        <button>Voir</button>
+      </div>
+    </li>
+    <!-- <ul>
+      {{ post }}
+    </ul> -->
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import {ref, onMounted} from 'vue'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup () {
+    const posts = ref([]);
+    // const test = () => {
+    //   router.push(`/posts/:${posts.value}`);
+    // };
+    onMounted(async () => {
+      try {
+        await axios
+          .get("https://jsonplaceholder.typicode.com/posts")
+          .then((response) => {
+            console.log(response.data);
+            console.log("ok");
+            posts.value = response.data;
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    return {
+      posts,
+    };
+    // const post = ref([])
+
+    // const loadPost = async () => {
+    //   try {
+    //     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    //     post.value = response
+    //   } catch(err) {
+    //     console.log(err);
+    //   }
+    // }
+    // loadPost()  
+
+
+
+
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    // .then(response => {
+    //   // handle repose
+    //   post.value = response
+    // })
+    // return {
+    //   post
+    // }
   }
+  // faire un onMounted
 }
 </script>
 
