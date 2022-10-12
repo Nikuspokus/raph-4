1c12f519a79fc16776fba63fabcc1907b8fcae3c
nmarquillier <nmarquillier@norauto.com>
2022-10-12 22:56:46 +0200

    postDetails - CSS

--- src/components/TitleApp.vue
+++ src/components/TitleApp.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+// export default {
+//   name: "TitleApp",
+//   props: {
+//     msg: String,
+//   },
+// };
+import router from "@/router";
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
 export default {
-  name: "TitleApp",
+  name: "PostList",
   props: {
     msg: String,
+  },
+  setup() {
+    const posts = ref([]);
+
+    const showIdPost = (post) => {
+      router.push({ name: "postdetails", params: { id: post.id } });
+    };
+
+    onMounted(async () => {
+      try {
+        await axios
+          .get("https://jsonplaceholder.typicode.com/posts")
+          .then((response) => {
+            posts.value = response.data;
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    return {
+      posts,
+      showIdPost,
+    };
   },
 };
 </script>
--- src/views/PostDetails.vue
+++ src/views/PostDetails.vue
@@ -1,20 +1,88 @@
 <template>
   <div id="postdetails">
+    <div style="width: 650px; margin-left: auto; margin-right: auto">
+      <ol class="list-post" v-for="post in posts" :key="post.id">
+        <div>
+          <table class="table-line">
+            {{
+              post.title
+            }}
+            <!-- <router-link :to="`/post/${post.id}`"> -->
+            <button @click="showIdPost(post)">Voir</button>
+            <!-- <button>Voir</button> -->
+          </table>
+        </div>
+      </ol>
+    </div>
+    <div style="width: 650px; margin-left: auto; margin-right: auto">
+      <table class="table-line">
+        <p>
+          <strong>sdfsf</strong>
+          qsdqsdqsdqsdqsdqsdqsdqsd
+        </p>
+      </table>
+    </div>
     <router-link to="/">Back</router-link>
   </div>
 </template>
 
 <script>
 import router from "@/router";
+import axios from "axios";
+import { ref, onMounted } from "vue";
 export default {
   name: "PostDetails",
   setup() {
+    const posts = ref([]);
     const showIdPost = () => {
       router.push({ name: "home" });
     };
+    onMounted(async () => {
+      try {
+        await axios
+          .get("https://jsonplaceholder.typicode.com/posts/:id")
+          .then((response) => {
+            posts.value = response.data;
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    });
     return {
       showIdPost,
     };
   },
 };
 </script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.table-line > p {
+  width: 650px;
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+  border: 1px solid grey;
+  border-radius: 6px;
+  padding: 10px;
+}
+
+.list-post {
+  justify-content: center;
+  margin: auto;
+  padding: 10px;
+}
+</style>
